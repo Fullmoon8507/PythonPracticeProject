1d1c61c9c2706e434b70c68224eac36e47bc820d
fullmoon8507 <fullmoon_8507@yahoo.co.jp>
2016-03-05 13:28:18 +0900

    IF文のサンプル追加

---
+++ base/if_test.py
@@ -0,0 +1,12 @@
+if __name__ == "__main__":
+
+    a = 2
+    if a%15 == 0:
+        print("FizzBuzz")
+    elif a%5 == 0:
+        print("Buzz")
+    elif a%3 == 0:
+        print("Fizz")
+    else:
+        print(a)
+
