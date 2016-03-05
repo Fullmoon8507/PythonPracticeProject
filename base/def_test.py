522af8c7a8412605fe559055d675526ed452de49
fullmoon8507 <fullmoon_8507@yahoo.co.jp>
2016-03-05 13:39:28 +0900

    関数定義のサンプルを追加

---
+++ base/def_test.py
@@ -0,0 +1,8 @@
+def add(x, y):
+    print(x+y)
+
+if __name__ == "__main__":
+    x = 4
+    y = 5
+    print(str(x) + " + " + str(y) + " = ")
+    add(4, 5)
---
+++ base/def_test2.py
@@ -0,0 +1,8 @@
+def add(x, y):
+    #print(x+y)
+    return x+y
+
+if __name__ == "__main__":
+    x = 4
+    y = 5
+    print(str(x) + " + " + str(y) + " = " + str(add(x, y)))
