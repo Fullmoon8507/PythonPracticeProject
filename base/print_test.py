da16b3ec66da2eb2f7dcdde70228c8d4d6cb0132
fullmoon8507 <fullmoon_8507@yahoo.co.jp>
2016-03-06 12:03:30 +0900

    print関数のサンプルを追加

---
+++ base/print_test.py
@@ -0,0 +1,12 @@
+if __name__ == "__main__":
+    print("Hello! Python World!")
+
+    str = "Python"
+    print("Hello! {0} World!".format(str))
+
+    str1 = "Python"
+    str2 = "World!"
+    print("Hello! {0} {1}".format(str1, str2))
+
+    str = "Hello! {0} {1}"
+    print(str.format(str1, str2))
