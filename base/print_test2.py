baf43271d67fcb0728c84d8899fcb4788179bfb5
fullmoon8507 <fullmoon_8507@yahoo.co.jp>
2016-03-06 17:11:29 +0900

    print関数のサンプルを追加

---
+++ base/print_test2.py
@@ -0,0 +1,10 @@
+if __name__  == "__main__":
+
+    print("xxx");
+    print("yyy");
+    print("zzz");
+
+    #「end」で行末の改行を違う文字に入れ替える
+    print("xxx", end='');
+    print("yyy", end='');
+    print("zzz");
