41673c2edc8ada64d97ea1b45a05b1ecd0bfa663
fullmoon8507 <fullmoon_8507@yahoo.co.jp>
2016-03-07 23:03:44 +0900

    アクセス制限のサンプルを追加

---
+++ apply/access_restriction.py
@@ -0,0 +1,27 @@
+class MyClass:
+    def __init__(self):
+        self.name = "tanaka"
+        self._name = "yamada"
+        self.__name = "suzuki"
+
+    def hello(self):
+        print("hello")
+
+    def _hello(self):
+        print("hello")
+
+    def __hello(self):
+        print("hello")
+
+if __name__ == "__main__":
+    a = MyClass()
+
+    print(a.name)           # 参照できる
+    print(a._name)          # 参照できるが慣習的に参照しない
+    #print(a.__name)        # 参照できない（AttributeError例外）
+    print(a._MyClass__name) # _クラス名__変数名でアクセスできる
+
+    a.hello()               # 参照できる
+    a._hello()              # 参照できるが慣習的に参照しない
+    #a.__hello()            # 参照できない（AttributeError例外）
+    a._MyClass__hello()     # _クラス名__変数名でアクセスできる
