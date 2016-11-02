d0aa260f20334bcf9d765916ba354a07a0c6e3a8
fullmoon8507 <you@example.com>
2016-11-03 04:14:32 +0900

    デザインパターンSingletonを追加。

---
+++ desing_pattern/singleton/main.py
@@ -0,0 +1,16 @@
+from singleton import Singleton
+
+
+def main():
+    s1 = Singleton('aaa')
+    s2 = Singleton('bbb')
+
+    if s1 is s2:
+        print(id(s1), id(s2))
+        print(s1.get_name(), s2.get_name())
+    else:
+        print('s1 is NOT s2')
+
+
+if __name__ == "__main__":
+    main()
