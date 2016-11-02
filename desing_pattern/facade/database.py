3a8ca2f2c74939d0e917901fa431800ad087a5db
fullmoon8507 <you@example.com>
2016-11-03 04:51:19 +0900

    デザインパターンFacadeを追加。

---
+++ desing_pattern/facade/database.py
@@ -0,0 +1,16 @@
+from configparser import ConfigParser
+import logging
+
+
+class Database():
+
+    def get_properties(self, dbname):
+        filename = dbname + '.ini'
+        conf = ConfigParser()
+
+        try:
+            conf.read(filename)
+            prob = conf['TEST1']
+            return prob
+        except IOError:
+            logging.exception('Warning' + filename + 'is not found.')
---
+++ desing_pattern/facade/main.py
@@ -0,0 +1,10 @@
+from page_maker import PageMaker
+
+
+def main():
+    pm = PageMaker()
+    pm.make_welcome_page('hyuki@hyuki.com', 'welcome.html')
+
+
+if __name__ == "__main__":
+    main()
